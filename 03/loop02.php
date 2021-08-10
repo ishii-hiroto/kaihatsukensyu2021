e419769b09641407133e4283d6c0ed226d49d08e
ishii <ishii-hiroto@webrage.jp>
2021-08-10 10:38:13 +0900

    [add] 行数と列数を指定してテーブル作成可能なコード
    必須項目のため

---
+++ 03/loop02.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+    </head>
+    <body>
+        <form method='get' action='loop02.php'>
+            <input type="number" name="cols0" >行×
+            <input type="number" name="cols1" >行<br>
+            <input type="submit" name="button" >
+            <input type="reset" name="button2" >
+        </form>
+        <table border="1">
+            <?php
+                for($i=0; $i < $_GET['cols0']; $i++){
+                    echo "<tr>";
+                    for($r=0; $r < $_GET['cols1']; $r++){
+                        echo "<td>佐野</td>";
+                    }
+                    echo "</tr>";
+                }
+            ?>
+        </table>
+    </body>
+</html>
